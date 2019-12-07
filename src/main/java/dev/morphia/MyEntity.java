bcfda14d852eb824c24f1bf4855b9097c5a8488d
Samuel Dalichampt <samuel@dataraxys.com>
2019-12-07 15:58:46 +0100

    add a testcase for https://github.com/MorphiaOrg/morphia/issues/1406

--- src/main/java/dev/morphia/MyEntity.java
+++ src/main/java/dev/morphia/MyEntity.java
@@ -1,7 +1,30 @@
 package dev.morphia;
 
 import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import org.bson.types.ObjectId;
 
 @Entity
 public class MyEntity {
+
+    @Id
+    private ObjectId id;
+
+    private int number;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
--- src/test/java/dev/morphia/ReproducerTest.java
+++ src/test/java/dev/morphia/ReproducerTest.java
@@ -1,9 +1,98 @@
 package dev.morphia;
 
+import com.mongodb.Block;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Projections;
+import org.bson.Document;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+
 
 public class ReproducerTest extends TestBase {
     @Test
     public void reproduce() {
+
+        // insert a test entity
+        MyEntity myEntity = new MyEntity();
+        myEntity.setNumber(10);
+        getDs().save(myEntity);
+
+
+        getDatabase().getCollection("MyEntity" ).aggregate(
+                Arrays.asList(
+                        Aggregates.project(
+                                Projections.fields(
+                                        Projections.include("number" )
+                                )
+                        )
+                )
+        ).forEach((Block<? super Document>) document -> {
+            // after save number is a int32
+             document.getInteger("number");
+
+            try {
+                document.getLong("number");
+                Assert.fail("number is not au long");
+            } catch (Exception e) {
+                //  must fail
+            }
+        });
+
+        // inc is OK and don't change field type
+        getDs().findAndModify(
+                getDs().createQuery(MyEntity.class).field("_id").equal(myEntity.getId()),
+                getDs().createUpdateOperations(MyEntity.class).inc("number", 2)
+        );
+
+        getDatabase().getCollection("MyEntity" ).aggregate(
+                Arrays.asList(
+                        Aggregates.project(
+                                Projections.fields(
+                                        Projections.include("number" )
+                                )
+                        )
+                )
+        ).forEach((Block<? super Document>) document -> {
+            // after increment, number is a int32
+            document.getInteger("number" );
+
+            try {
+                document.getLong("number");
+
+                Assert.fail("number is not au long");
+            } catch (Exception e) {
+                //  must fail
+            }
+        });
+
+        // dec is not OK and change field type from int32 to int64
+        getDs().findAndModify(
+                getDs().createQuery(MyEntity.class).field("_id").equal(myEntity.getId()),
+                getDs().createUpdateOperations(MyEntity.class).dec("number", 2)
+        );
+
+        getDatabase().getCollection("MyEntity" ).aggregate(
+                Arrays.asList(
+                        Aggregates.project(
+                                Projections.fields(
+                                        Projections.include("number" )
+                                )
+                        )
+                )
+
+        ).forEach((Block<? super Document>) document -> {
+            // after decrement, number is now a long
+            try {
+                document.getInteger("number");
+
+                Assert.fail("number is not au int");
+            } catch (Exception e) {
+                //  must fail
+            }
+            document.getLong("number" );
+        });
+
     }
 }
